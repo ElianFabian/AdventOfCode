a2f562238c8f2806bc7d7b366e2dfb2a7547e0f0
Elián Fabián El Belazi <elian.fabian.el.belaizi@gmail.com>
2023-01-23 22:44:03 +0100

    Update PuzzleYear2015Day4.kt

--- src/main/kotlin/elianfabian/adventofcode/year2015/PuzzleYear2015Day4.kt
+++ src/main/kotlin/elianfabian/adventofcode/year2015/PuzzleYear2015Day4.kt
@@ -64,7 +64,7 @@ private val PuzzlePartTwo = object : Puzzle<String, Int>
 
 //region Utils
 
-fun md5(input: String): String
+private fun md5(input: String): String
 {
     val md = MessageDigest.getInstance("MD5")
     return BigInteger(1, md.digest(input.toByteArray())).toString(16).padStart(32, '0')
