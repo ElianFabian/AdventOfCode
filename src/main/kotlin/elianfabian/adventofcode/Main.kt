f02d777708623fa4e957c6a812532e589c27f5fe
Elián Fabián El Belazi <elian.fabian.el.belaizi@gmail.com>
2023-01-24 23:45:40 +0100

    Update Main.kt

--- src/main/kotlin/elianfabian/adventofcode/Main.kt
+++ src/main/kotlin/elianfabian/adventofcode/Main.kt
@@ -1,24 +1,36 @@
 package elianfabian.adventofcode
 
 import elianfabian.adventofcode.util.showPuzzleResults
-import elianfabian.adventofcode.year2015.Year2015Day1Puzzle
-import elianfabian.adventofcode.year2015.Year2015Day2Puzzle
-import elianfabian.adventofcode.year2015.Year2015Day3Puzzle
-import elianfabian.adventofcode.year2015.Year2015Day4Puzzle
+import elianfabian.adventofcode.year2015.*
 
+
+/**
+ * Hi there, I'm Elián and in this project it's where I complete the Advent of Code puzzles in Kotlin.
+ * 
+ * The solutions I give in every puzzle I try to make them easy to read and understand rather than being the most efficient way to do it
+ * since they're just simple puzzles.
+ * 
+ * In case you found this repository I hope my solutions can be interesting or helpful for you.
+ */
 fun main()
 {
     showLastPuzzle()
     //showAllPuzzlesOfAllTheYears()
 }
 
-
+/**
+ * In here we have a variable with all the puzzles group by year and day.
+ * I don't like to do it this way, but I haven't found any better way at the moment,
+ * one way could be to make use of reflection and source generation, but I would prefer
+ * to avoid that kind of solution, also it's kind of difficult to deal with it with my actual knowledge.
+ */
 val puzzlesGroupByYearAndDay = mapOf(
     2015 to mapOf(
         1 to Year2015Day1Puzzle,
         2 to Year2015Day2Puzzle,
         3 to Year2015Day3Puzzle,
         4 to Year2015Day4Puzzle,
+        4 to Year2015Day5Puzzle,
     ),
 )
 
